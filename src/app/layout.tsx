645aac1b9aaff3feaf844a2c51f1a5415c54bfd8
tiderbrandt <tobbe@borasbs.se>
2025-09-05 11:03:59 +0200

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,17 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Västra Götalands Bågskytteförbund',
   description: 'Officiell webbplats för Västra Götalands Bågskytteförbund (VGBF)',
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+  },
+  manifest: '/site.webmanifest',
 }
 
 export default function RootLayout({
